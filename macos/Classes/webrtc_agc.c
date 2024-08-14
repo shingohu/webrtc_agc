b780cf19cb6776a9b6fec16a7315bb800d73fd23
shingohu <shingohu@163.com>
2024-08-14 13:53:09 +0800

    增加macos平台

---
+++ macos/Classes/webrtc_agc.c
@@ -0,0 +1,4 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../webrtc_agc.podspec for more information.
+#include "../../src/webrtc_agc.c"
+#include "../../src/agc.c"
